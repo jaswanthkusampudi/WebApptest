f9237025abd4a69d91a10318e69158c32d11c4fa
Jaswanth Varma Kusampudi <jaswanthvarma1224662@gmail.com>
2026-09-14 18:23:08 +0530

    Message change code

--- src/test/java/automationtest/FirstTest.java
+++ src/test/java/automationtest/FirstTest.java
@@ -22,7 +22,7 @@ public class FirstTest extends BaseClass {
 		electronics.addProductToCart();
 		cart.selectCountry();
 		cart.checkout();
-		System.out.println("Login Test");
+		System.out.println("Good Morning");
 		closeBrowser();
 		
 		
