dc999793e31b77da030ad16dc8f5efac6c8da3f6
Jaswanth Varma Kusampudi <jaswanthvarma1224662@gmail.com>
2026-09-15 10:45:21 +0530

    Implemented JIRA-123

--- src/test/java/automationtest/FirstTest.java
+++ src/test/java/automationtest/FirstTest.java
@@ -22,7 +22,7 @@ public class FirstTest extends BaseClass {
 		electronics.addProductToCart();
 		cart.selectCountry();
 		cart.checkout();
-		System.out.println("Good afternoon!!");
+		System.out.println("Good evening");
 		closeBrowser();
 		
 		
