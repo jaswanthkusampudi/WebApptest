54ca8661142fa260bfef70966989abe8c85bfddd
Jaswanth Varma Kusampudi <jaswanthvarma1224662@gmail.com>
2026-09-15 10:31:28 +0530

    Updated FirstTest msgs

--- src/test/java/automationtest/FirstTest.java
+++ src/test/java/automationtest/FirstTest.java
@@ -22,7 +22,7 @@ public class FirstTest extends BaseClass {
 		electronics.addProductToCart();
 		cart.selectCountry();
 		cart.checkout();
-		System.out.println("Hello");
+		System.out.println("Good one");
 		closeBrowser();
 		
 		
