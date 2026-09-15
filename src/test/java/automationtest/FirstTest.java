375b78d306734e38af50e87b8fded929726f9e9b
Jaswanth Varma Kusampudi <jaswanthvarma1224662@gmail.com>
2026-09-15 12:07:50 +0530

    Code changes

--- src/test/java/automationtest/FirstTest.java
+++ src/test/java/automationtest/FirstTest.java
@@ -22,7 +22,7 @@ public class FirstTest extends BaseClass {
 		electronics.addProductToCart();
 		cart.selectCountry();
 		cart.checkout();
-		System.out.println("Good Eve");
+		System.out.println("Good Eveni");
 		closeBrowser();
 		
 		
